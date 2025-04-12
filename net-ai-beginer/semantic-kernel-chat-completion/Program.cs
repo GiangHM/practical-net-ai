f01f28afa69e16f5f9e7c5466651e69b7ceab4b2
GiangHM <hoang-giang.mai@bollore.com>
2025-04-12 15:25:34 +0700

    Add proj to learn sematic kernel

---
+++ net-ai-beginer/semantic-kernel-chat-completion/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.SemanticKernel;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+// AddOllamaChatCompletion
+// There are some overload methods
+// We can pass a custom HttpClient to AddOllamaChatCompletion
+#pragma warning disable SKEXP0070
+builder.Services.AddOllamaChatCompletion(
+    modelId: "",
+    endpoint: new Uri("")
+    );
+
+builder.Services.AddTransient((serviceProvider) =>
+{
+    return new Kernel(serviceProvider);
+});
+Console.WriteLine("Hello, World!");
