69247db9689480536f3435650b319a0664c843c4
GiangHM <36400582+GiangHM@users.noreply.github.com>
2025-09-07 11:18:53 +0700

    Re-organize solution by grouping projects into new folders: MEAI, semantic-kernel

--- net-ai-beginer/practice-sk-azurefoundrylocal/Program.cs
+++ net-ai-beginer/practice-sk-azurefoundrylocal/Program.cs
@@ -1,34 +1,113 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
-using System.Net;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
 
-Console.WriteLine("Hello, I'm learning AI");
 
 string filePath = Path.GetFullPath("appsettings.json");
 var config = new ConfigurationBuilder()
     .AddJsonFile(filePath)
     .Build();
+
 // Create a kernel with Azure OpenAI chat completion
-var deploymentName = config["DeploymentName"];
-var endpoint = config["Endpoint"];
+// modelId = deploymentName
+var modelId = config["DeploymentName"] ?? "Phi-3.5-mini-instruct-generic-cpu";
+var endpoint = config["Endpoint"] ?? "";
 
+// Create a kernel and chat completion service
 var builder = Kernel.CreateBuilder();
-builder.AddOpenAIChatCompletion("Phi-3.5-mini-instruct-cuda-gpu", new Uri(" http://127.0.0.1:51560/"), null);
-var kernel = builder.Build();
-var chat = kernel.GetRequiredService<IChatCompletionService>();
 
-// Start a chat loop
-Console.WriteLine("Chat with the Semantic Kernel! Type 'exit' to quit.");
-var history = new ChatHistory();
-while (true)
+//AddAzureOpenAIChatCompletion => to interact with Azure OpenAI Service| Azure Foundry Service
+builder.AddOpenAIChatCompletion(modelId, new Uri(endpoint), null);
+var kernel = builder.Build();
+
+#region 1-  Use the chat completion service
+//var chat = kernel.GetRequiredService<IChatCompletionService>();
+
+//// Start a chat loop
+//Console.WriteLine("Chat with the Semantic Kernel! Type 'exit' to quit.");
+//var history = new ChatHistory();
+//string prompt = $"""
+//You are a highly experienced software engineer. Explain the concept of asynchronous programming to a beginner.
+//""";
+//while (true)
+//{
+//    //Console.Write("You: ");
+//    //var userInput = Console.ReadLine();
+//    //if (string.Equals(userInput, "exit", StringComparison.OrdinalIgnoreCase)) break;
+//    history.AddUserMessage(prompt);
+//    var response = await chat.GetChatMessageContentAsync(history);
+//    Console.WriteLine($"Bot: {response}");
+//    history.AddAssistantMessage(response.ToString());
+//}
+#endregion
+
+#region 2 - Use the Handlebars template engine
+
+
+// Alternative way, you can use Handlebars templates in YAML prompts
+const string handlebarsTemplate = """
+         <message role="system">
+         Instructions: You are a career advisor. Analyze the skill gap between 
+         the user's current skills and the requirements of the target role.
+         </message>
+         <message role="user">Target Role:{{targetRole}} </message>
+         <message role="user">Current Skills: {{currentSkills}}</message>
+
+         <message role="assistant">
+         "Skill Gap Analysis":
+         {
+             "missingSkills": [],
+             "coursesToTake": [],
+             "certificationSuggestions": []
+         }
+         </message>
+         """;
+
+var templateFactory = new HandlebarsPromptTemplateFactory();
+var promptTemplateConfigs = new PromptTemplateConfig()
 {
-    Console.Write("You: ");
-    var userInput = Console.ReadLine();
-    if (string.Equals(userInput, "exit", StringComparison.OrdinalIgnoreCase)) break;
-    history.AddUserMessage(userInput);
-    var response = await chat.GetChatMessageContentAsync(history);
-    Console.WriteLine($"Bot: {response}");
-    history.AddAssistantMessage(response.ToString());
+    Template = handlebarsTemplate,
+    TemplateFormat = "handlebars",
+    Name = "MissingSkillsPrompt",
+};
+
+// arguments can be an user input
+var kernelArguments = new KernelArguments
+{
+    ["targetRole"] = "Game Developer",
+    ["currentSkills"] = "Software Engineering, C#, Python, Drawing, Guitar, Dance"
+};
+
+
+var renderedPromptTemplate = await templateFactory.Create(promptTemplateConfigs)
+    .RenderAsync(kernel, kernelArguments);
+
+Console.WriteLine($"Rendered Prompt Template: {renderedPromptTemplate}");
+
+
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+var chatHistory = new ChatHistory();
+
+chatHistory.AddUserMessage(renderedPromptTemplate);
+await GetReply();
+
+
+// Get a follow-up prompt from the user
+Console.WriteLine("Assistant: How can I help you?");
+Console.Write("User: ");
+string input = Console.ReadLine()!;
+chatHistory.AddUserMessage(input);
+await GetReply();
+
+async Task GetReply()
+{
+    ChatMessageContent chatContent = await chat.GetChatMessageContentAsync(
+    chatHistory: chatHistory,
+    kernel: kernel);
+
+    Console.WriteLine($"Assistant Response: {chatContent}");
+    chatHistory.AddAssistantMessage(chatContent.ToString());
+
 }
+#endregion
