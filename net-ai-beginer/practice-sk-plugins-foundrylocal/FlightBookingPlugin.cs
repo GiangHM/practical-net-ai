484217ee805950d0874e4dffbf3bb93b64b62e09
GiangHM <36400582+GiangHM@users.noreply.github.com>
2025-09-08 07:26:38 +0700

    semantic kernel

---
+++ net-ai-beginer/practice-sk-plugins-foundrylocal/FlightBookingPlugin.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace practice_sk_plugins_foundrylocal
+{
+    public class FlightBookingPlugin
+    {
+        private const string FilePath = "flightsData.json";
+        private List<FlightModel> flights;
+
+        public FlightBookingPlugin()
+        {
+            // Load flights from the file
+            flights = LoadFlightsFromFile();
+        }
+
+        // Create a plugin function with kernel function attributes
+        [KernelFunction("search_flights")]
+        [Description("Search for available flights based on destination and departure date in format YYYY-MM-DD")]
+        public IEnumerable<FlightModel> SearchFlights(string destination, string departureDate)
+        {
+            var results = flights.Where(f => f.Destination.Equals(destination, StringComparison.OrdinalIgnoreCase)
+                                          && f.DepartureDate.Equals(departureDate, StringComparison.OrdinalIgnoreCase)
+                                          && !f.IsBooked).ToList();
+            return results;
+        }
+
+
+        // Create a kernel function to book flights
+        [KernelFunction("book_flight")]
+        [Description("Books a flight based on the flight ID provided")]
+        [return: Description("Booking confirmation message")]
+        public string BookFlight(int flightId)
+        {
+            // Add logic to book a flight
+            var flight = flights.FirstOrDefault(f => f.Id == flightId);
+            if (flight == null)
+            {
+                return "Flight not found. Please provide a valid flight ID.";
+            }
+
+            if (flight.IsBooked)
+            {
+                return $"You've already booked this flight.";
+            }
+
+            flight.IsBooked = true;
+            SaveFlightsToFile();
+
+            return @$"Flight booked successfully! Airline: {flight.Airline}, 
+                     Destination: {flight.Destination}, 
+                     Departure: {flight.DepartureDate}, 
+                     Price: ${flight.Price}.";
+        }
+
+
+        private void SaveFlightsToFile()
+        {
+            var json = JsonSerializer.Serialize(flights, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(FilePath, json);
+        }
+
+        private List<FlightModel> LoadFlightsFromFile()
+        {
+            if (File.Exists(FilePath))
+            {
+                var json = File.ReadAllText(FilePath);
+                return JsonSerializer.Deserialize<List<FlightModel>>(json)!;
+            }
+
+            throw new FileNotFoundException($"The file '{FilePath}' was not found. Please provide a valid flights.json file.");
+        }
+    }
+
+    // Flight model
+    public class FlightModel
+    {
+        public int Id { get; set; }
+        public required string Airline { get; set; }
+        public required string Destination { get; set; }
+        public required string DepartureDate { get; set; }
+        public decimal Price { get; set; }
+        public bool IsBooked { get; set; } = false; // Added to track booking status
+    }
+}
---
+++ net-ai-beginer/practice-sk-plugins-foundrylocal/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using practice_sk_plugins_foundrylocal;
+
+
+string filePath = Path.GetFullPath("appsettings.json");
+var config = new ConfigurationBuilder()
+    .AddJsonFile(filePath)
+    .Build();
+
+// Create a kernel with Azure OpenAI chat completion
+// modelId = deploymentName
+var modelId = // "gpt-4o-minigpt-4o-mini";
+ config["DeploymentName"] ?? "Phi-3.5-mini-instruct-generic-cpu";
+var endpoint = //"https://models.inference.ai.azure.com"; 
+config["Endpoint"] ?? "";
+
+// Create a kernel and chat completion service
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(modelId, new Uri(endpoint), null);
+var kernel = builder.Build();
+
+// Add plugins to kernel
+kernel.Plugins.AddFromType<FlightBookingPlugin>("FlightBookingPlugin");
+
+KernelFunction searchFlights = kernel.Plugins.GetFunction("FlightBookingPlugin", "search_flights");
+
+OpenAIPromptExecutionSettings settings = new()
+{
+    //MaxTokens = 1000,
+    //Temperature = 0.7,
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Required(functions: [searchFlights])
+};
+// Start a chat loop
+var history = new ChatHistory();
+history.AddSystemMessage("You're flight booking assistant");
+
+AddUserMessage("Find me a flight to Tokyo on the 2026-01-19");
+await GetReply();
+GetInput();
+await GetReply();
+
+
+void GetInput()
+{
+    Console.Write("User: ");
+    string input = Console.ReadLine()!;
+    history.AddUserMessage(input);
+}
+
+async Task GetReply()
+{
+    var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+    ChatMessageContent reply = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: settings,
+        kernel: kernel
+    );
+    Console.WriteLine("Assistant: " + reply.ToString());
+    history.AddAssistantMessage(reply.ToString());
+}
+
+void AddUserMessage(string msg)
+{
+    Console.WriteLine("User: " + msg);
+    history.AddUserMessage(msg);
+}
